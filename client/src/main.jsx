f6dbb844ea39a18fd07a0f5b7dddcba15e4d23e3
wichayut <6711130010@mut.ac.th>
2026-09-25 00:59:01 +0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -23,43 +23,26 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
-<<<<<<< HEAD
     path: "/register",
     element: <Register />,
   },
   {
     path: "/employee",
     element: (
-      <ProtectedRoute requiredPermIndex={0}>
-=======
-    // สมมติว่าหน้า Employee ของคุณใช้จัดการพนักงาน ซึ่งเทียบเท่ากับ Profile หรือ Dashboard ในตาราง
-    // คุณก็เลือกรหัสที่ตรงกันมาใส่ (ตัวอย่างนี้ขอสมมติเป็น S04 - Profile)
-    path: "/employee",
-    element: (
       <ProtectedRoute requiredScreenCode="S04">
->>>>>>> adf18f6815e3b6cdaa2f92586e302d513cf37ff2
         <Employee />
       </ProtectedRoute>
     ),
   },
   {
-    // สมมติว่าหน้า Customer คือหน้าเกี่ยวกับการจอง
     path: "/customer",
     element: (
-<<<<<<< HEAD
-      <ProtectedRoute requiredPermIndex={1}>
-=======
       <ProtectedRoute requiredScreenCode="S02">
->>>>>>> adf18f6815e3b6cdaa2f92586e302d513cf37ff2
         <Customer />
       </ProtectedRoute>
     ),
   },
   {
-<<<<<<< HEAD
-=======
-    // สมมติว่าหน้า Product คือหน้าเกี่ยวกับเส้นทาง
->>>>>>> adf18f6815e3b6cdaa2f92586e302d513cf37ff2
     path: "/product",
     element: (
       <ProtectedRoute requiredScreenCode="S03">
@@ -73,7 +56,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-<<<<<<< HEAD
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
@@ -83,10 +65,3 @@ if (rootElement) {
     </React.StrictMode>
   );
 }
-=======
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
->>>>>>> adf18f6815e3b6cdaa2f92586e302d513cf37ff2
